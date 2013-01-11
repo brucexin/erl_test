d6f67ddb992a930c8bbe86b6eedb7ca131d12c4d
Bruce Xin <bruce.xin@gmail.com>
2013-01-11 12:02:33 +0800

    use code_change upgrade internal state

--- src/testserver.erl
+++ src/testserver.erl
@@ -17,12 +17,15 @@ start_link() ->
 stop() ->
     gen_fsm:cast(?MODULE, stop).
 
+query_state() ->
+    gen_fsm:sync_send_all_state_event(?MODULE, query_state).
+
 
 %% server functions
 init([]) ->
     ?LOGGER("testserver started! ~p~n", [application:get_all_key()]),
     {Name, Desc, Vsn} = get_app_vsn(),
-    {ok, state_print, [Name, Vsn], ?WAIT_TIMEOUT}.
+    {ok, state_print, [], ?WAIT_TIMEOUT}.
 
 terminate(Reason, StateName, _LoopData) ->
     ?LOGGER("testserver terminate ~p for ~p~n!", [Reason, StateName]).
@@ -31,14 +34,25 @@ get_app_vsn() ->
     {ok, AppName} = application:get_application(self()),
     lists:keyfind(AppName, 1, application:loaded_applications()).
 
-state_print(timeout, LoopData) ->
-    %AppName = lists:nth(1, LoopData),
-    %Vsn = lists:nth(2, LoopData),
-    {AppName, Desc, Vsn} = get_app_vsn(),
+state_print(timeout, []) ->
+    ?LOGGER("None state ~n"),
+    {next_state, state_print, [], ?WAIT_TIMEOUT};
+state_print(timeout, [{AppName, Desc, Vsn}]) ->
     ?LOGGER("testserver for ~p-~p alive at ~p~n", [AppName, Vsn, erlang:now()]),
-    {next_state, state_print, LoopData, ?WAIT_TIMEOUT}.
+    {next_state, state_print, [{AppName, Desc, Vsn}], ?WAIT_TIMEOUT}.
 
 handle_info(stop, _StateName, LoopData) ->
     {stop, normal, LoopData}.
+
+handle_sync_event(query_state, _From, StateName, LoopData) ->
+    {reply, LoopData, StateName, ?WAIT_TIMEOUT}.
+
+code_change({down, _Vsn}, StateName, LoopData, _Extra) ->
+    ?LOGGER("code change down for testserver ~p ~p ~n", [_Vsn, StateName]),
+    {ok, StateName, []};
+code_change(_Vsn, StateName, LoopData, _Extra) ->
+    ?LOGGER("code change upgrade for testserver ~p ~p ~n", [_Vsn, StateName]),
+    {Name, Desc, Vsn} = get_app_vsn(),
+    {ok, StateName, [{Name, Desc, Vsn}]}.
 
 
